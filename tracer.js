3eb622c3d4f56508dcd4bbdaa5b762ec016298eb
Joe Teglasi <teglasij@me.com>
2026-01-12 14:47:29 -0500

    added JSDoc annotations for tracer

--- tracer.js
+++ tracer.js
@@ -11,16 +11,40 @@
  * // output: YYYY-MM-DDTHH:mm:ss.sss <INFO> myfile.js: This is an info message
  */
 
+/**
+ * @function getDateTime
+ * @description Returns the current date and time in ISO format.
+ * @returns {string} The current date and time in ISO format.
+ */
+
+
 function getDateTime() {
     return new Date().toLocalISOString();
 }
 
-const tracer = function (file_name) {
+/**
+ * @external Serial
+ * @class
+ * @description See https://www.espruino.com/Reference#Serial
+ * @method inject
+ * @param {string} text The string to inject into the Serial interface.
+ * @returns {void}
+ */
+
+/**
+ * @function tracer
+ * @description A simple logging utility that prefixes log messages with timestamps, source file name, and log levels.
+ * @param {string} file_name The name of the currently executing file, to include it in log messages
+ * @param {Serial} [serial] An optional Serial object to inject log messages into, e.g. LoopbackB. Useful for streaming logs
+ * @returns {Object} An object containing logging methods: log, info, debug, warn, and error.
+ */
+
+const tracer = function (file_name, serial) {
     return ["log", "info", "debug", "warn", "error"].reduce(function (logger, method) {
         logger[method] = function () {
             const log_text = `${getDateTime()} <${method.toUpperCase()}> ${file_name}: ` + Object.values(arguments).map(function (x) { return (typeof x === "object") ? JSON.stringify(x) : x }).join(" ");
             console.log(log_text);
-            LoopbackB.inject(log_text+'\n');
+            serial && serial.inject(log_text+'\n');
         };
         return logger;
     }, {});
